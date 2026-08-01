36fbeb4db5a1bb82e14ae771797b9c446bd1991b
txN1220 <et23016406@gmail.com>
2026-08-01 23:19:17 +0800

    Update try_1.py

--- pycharm項目/try_1.py
+++ pycharm項目/try_1.py
@@ -1,7 +1,8 @@
 import time
 seconds = 5
 while seconds > 0:
-    print(seconds, end='\r', flush=True)
+    print(seconds)
+    print(flush=True, end='\r')
     time.sleep(1)
     seconds -= 1
 print('we did it!')
