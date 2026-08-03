fbbec41c2ec57781028a7a5f69de81ff3689ba54
txN1220 <et23016406@gmail.com>
2026-08-03 21:29:24 +0800

    Add files via upload

---
+++ pycharm項目/基礎用法.py
@@ -0,0 +1,21 @@
+#求字串長度
+X = "fantastic"
+print(len(X))
+
+#索引的用法
+print(X[0])
+print(X[7])
+print(X[len(X) - 1])
+#布爾值
+a = True
+b= False
+
+#Nonetype
+c = None
+
+#試驗
+print(type(a))
+print(type(X))
+print(type(1.5))
+print(type(1.0))
+print(type(1))
